7e7badb83a19f89feba6f79d365b703a5bab50c8
Ashabul Elaph Hilful <86321389+Hilful@users.noreply.github.com>
2022-02-06 02:21:39 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,71 @@
-document.querySelector("h1").innerHTML="GoodBye!!!"
+// document.querySelectorAll("#list a");
+// NodeList []length: 0[[Prototype]]: NodeListentries: ƒ entries()forEach: ƒ forEach()item: ƒ item()keys: ƒ keys()length: (...)values: ƒ values()constructor: ƒ NodeList()Symbol(Symbol.iterator): ƒ values()Symbol(Symbol.toStringTag): "NodeList"get length: ƒ length()[[Prototype]]: Object
+// document.querySelectorAll("#list");
+// NodeList []length: 0[[Prototype]]: NodeList
+// document.querySelector("#list")
+// null
+// document.querySelectorAll("#list")
+// NodeList []length: 0[[Prototype]]: NodeList
+// document.querySelectorAll("#itemList")
+// NodeList []length: 0[[Prototype]]: NodeList
+// document.querySelectorAll(".itemList")
+// NodeList []length: 0[[Prototype]]: NodeList
+// document.querySelector(".itemList")
+// null
+// document.querySelector("#itemList")
+// null
+// document.querySelector("a").style.colr
+// undefined
+// document.querySelector("a").style.color = "Blue"
+// 'Blue'
+//  document.querySelector("h1").fontSize = "8rem";
+// '8rem'
+//  document.querySelector("h1").style.fontSize = "8rem";
+// '8rem'
+//  document.querySelector("h1").style.color = "yello";
+// 'yello'
+//  document.querySelector("h1").style.color = "yellow";
+// 'yellow'
+//  document.querySelector("button").style.backgroundColor = "pink";
+// 'pink'
+//  document.querySelector("button").classList.add("invisible");
+// undefined
+//  document.querySelector("button").classList.remove("invisible");
+// undefined
+//  document.querySelector("button").classList.toggle("invisible");
+// true
+//  document.querySelector("button").classList.toggle("invisible");
+// false
+
+
+
+
+
+
+// document.querySelector("h1").classList
+// DOMTokenList [value: '']length: 0value: ""[[Prototype]]: DOMTokenList
+// document.querySelector("h1").classList.add("huge");
+// undefined
+
+
+
+
+// document.querySelector("h1").innerHTML;
+// '<strong>Hello</strong>'
+// document.querySelector("h1").innerText;
+// 'Hello'
+// document.querySelector("h1").innerHTML = "<em>This is Right!!!</em>"
+// '<em>This is Right!!!</em>'
+
+
+
+// document.querySelector("a");
+// <a href=​"https:​/​/​www.google.com">​Google​</a>​
+// document.querySelector("a").attributes;
+// NamedNodeMap {0: href, href: href, length: 1}0: hrefhref: hreflength: 1[[Prototype]]: NamedNodeMap
+// document.querySelector("a").getAttribute;
+// ƒ getAttribute() { [native code] }
+// document.querySelector("a").getAttribute("href");
+// 'https://www.google.com'
+// document.querySelector("a").setAttribute("href","https://www.yahoo.com");
+// undefined
